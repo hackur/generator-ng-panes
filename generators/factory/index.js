6a249105a7c0d8da0bf7d76a37408c944383ff97
joelchu <joelckchu@gmail.com>
2015-08-20 00:29:36 +0800

    updating some of the styling in generator files

--- generators/factory/index.js
+++ generators/factory/index.js
@@ -9,7 +9,8 @@ var Generator = module.exports = function Generator() {
 
 util.inherits(Generator, ScriptBase);
 
-Generator.prototype.createServiceFiles = function createServiceFiles() {
+Generator.prototype.createServiceFiles = function createServiceFiles()
+{
   this.generateSourceAndTest(
     'service/factory',
     'spec/service',
