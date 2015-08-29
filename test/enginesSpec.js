d7e1eb47b74f8547273e5aac80a6402703b1f1b6
joelchu <joelckchu@gmail.com>
2015-08-29 17:30:32 +0800

    add back enginesSpec test spec

---
+++ test/enginesSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+var expect = require("chai").expect;
+var Engines = require("../lib/engines.js");
+
+describe("Engines", function()
+{
+	describe('#underscore' , function()
+	{
+		it("should replace the openning and closing tags" , function()
+		{
+			var source = "<% something %>";
+
+			
+		});
+	});
+});
