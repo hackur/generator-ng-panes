ea929ba63e463c39b677f8a91bb5c3531155ee71
joelchu <joelckchu@gmail.com>
2015-11-28 15:28:10 +0000

    update all the old tempalte to use IES

--- generators/templates/javascript/service/factory.js
+++ generators/templates/javascript/service/factory.js
@@ -1,23 +1,26 @@
-'use strict';
-
-/**
- * @ngdoc service
- * @name <%= scriptAppName %>.<%= cameledName %>
- * @description
- * # <%= cameledName %>
- * Factory in the <%= scriptAppName %>.
- */
-angular.module('<%= scriptAppName %>').factory('<%= cameledName %>', [function ()
+(function()
 {
-    // Service logic
-    // ...
-    var meaningOfLife = 42;
+    'use strict';
 
-    // Public API here
-    return {
-      someMethod: function ()
-      {
-          return meaningOfLife;
-      }
-    };
-}]);
+    /**
+     * @ngdoc service
+     * @name <%= scriptAppName %>.<%= cameledName %>
+     * @description
+     * # <%= cameledName %>
+     * Factory in the <%= scriptAppName %>.
+     */
+    angular.module('<%= scriptAppName %>').factory('<%= cameledName %>', [function ()
+    {
+        // Service logic
+        // ...
+        var meaningOfLife = 42;
+
+        // Public API here
+        return {
+          someMethod: function ()
+          {
+              return meaningOfLife;
+          }
+        };
+    }]);
+}());
--- generators/templates/javascript/service/service.js
+++ generators/templates/javascript/service/service.js
@@ -1,13 +1,22 @@
-'use strict';
-
-/**
- * @ngdoc service
- * @name <%= scriptAppName %>.<%= cameledName %>
- * @description
- * # <%= cameledName %>
- * Service in the <%= scriptAppName %>.
- */
-angular.module('<%= scriptAppName %>').service('<%= cameledName %>', [function ()
+(function()
 {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-}]);
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name <%= scriptAppName %>.<%= cameledName %>
+     * @description
+     * # <%= cameledName %>
+     * Service in the <%= scriptAppName %>.
+     */
+    angular.module('<%= scriptAppName %>').service('<%= cameledName %>', [function ()
+    {
+        // AngularJS will instantiate a singleton by calling "new" on this function
+        var self = this;
+        // inject your method into the self.yourMethod = function() {} etc
+
+        return self;
+    }]);
+
+}());
+
