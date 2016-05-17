f669b1ff3c4c59f68502bc8480d7e7784cd4c07c
Joel Chu <joel.chu@keytree.co.uk>
2016-05-17 09:40:13 +0100

    to v1.3.3

--- generators/templates/javascript/service/service.js
+++ generators/templates/javascript/service/service.js
@@ -12,10 +12,10 @@
     angular.module('<%= scriptAppName %>').service('<%= cameledName %>', function()
     {
         // AngularJS will instantiate a singleton by calling "new" on this function
-        var self = this;
+        var srv = this;
         // inject your method into the self.yourMethod = function() {} etc
 
-        return self;
+        return srv;
     });
 
 }());
